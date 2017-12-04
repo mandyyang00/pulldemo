272f10045dcd5ebfe7c990e111e5b0a1edf37a59
mandyyang00 <659890208@qq.com>
2017-12-04 13:23:17 +0800

    ul

--- src/aa.js
+++ src/aa.js
@@ -5,7 +5,18 @@ import React from 'react'
 class AA extends React.Component{
 	render(){
 		return(
-			<div>yangman</div>
+			<div>
+				<p>yangman</p>
+				<ul>
+					<li>11111</li>
+					<li>11111</li>
+					<li>11111</li>
+					<li>11111</li>
+					<li>11111</li>
+					<li>11111</li>
+					
+				</ul>
+			</div>
 		)
 	}
 }
