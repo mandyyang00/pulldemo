f5b5de7ab8bac2a2646186e5d2c92935f9096183
hopefullman <1206892998@qq.com>
2017-12-04 13:14:49 +0800

    bb.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AA from './aa' 
-
+import AA from './aa';
+import BB from './bb' 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <AA/>
+          <BB/>
       </div>
     );
   }
---
+++ src/bb.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+class BB extends React.Component{
+	render(){
+		return(
+			<div>ojvjdv</div>
+		)
+	}
+}
+
+export default BB
